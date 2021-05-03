fa91d98e9d830e0c8afaf71ec25d8df3a97b40cd
planeset94 <planeset94@gmail.com>
2021-05-03 23:55:52 +0200

    ADJ reset function

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -18,7 +18,9 @@ var ticketPriceEl = document.getElementById('ticket_price');
 
 
 var copyNameEl = document.getElementById('copy_name');
+var coachNumberEl = document.getElementById('coach_number');
 var codexEl = document.getElementById('codex');
+var offerTypeEl = document.getElementById('offer_type');
 
 
 var buttonEl_1 = document.getElementById('crea');
@@ -28,34 +30,41 @@ function random_Function(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function coach_Function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 
 buttonEl_1.addEventListener('click', function () {
     codexEl.innerHTML = random_Function(9000, 1000);
+    coachNumberEl.innerHTML = coach_Function(1, 20);
     copyNameEl.innerHTML = userNameEl.value;
 
 
     if (ageEl.value == "minorenne") {
-
+        offerTypeEl.innerHTML = "Sconto Minorenne";
         ticketPriceEl.innerHTML = (0.21 * distanceEl.value * 0.8).toPrecision(3) + "€";
     } else if (ageEl.value == "over65") {
+        offerTypeEl.innerHTML = "Sconto Over65";
         ticketPriceEl.innerHTML = (0.21 * distanceEl.value * 0.6).toPrecision(3) + "€";
     } else {
+        offerTypeEl.innerHTML = "Nessuno sconto";
         ticketPriceEl.innerHTML = (0.21 * distanceEl.value).toPrecision(3) + "€";
     }
-
-
-
-
-
 
 });
 
 buttonEl_2.addEventListener('click', function () {
     var conferma = confirm('Are you sure to reset data?');
     if (conferma) {
-        userName = "";
-        distance = "";
+        userNameEl.value = "";
+        distanceEl.value = "";
+        copyNameEl.innerHTML = "";
+        offerTypeEl.innerHTML = "";
+        coachNumberEl.innerHTML = "";
+        codexEl.innerHTML = "";
+        ticketPriceEl.innerHTML = "";
+
     }
 
 });
